c26f2b5688e81ac15a8321ab6f10a6baba9902eb
alex-iskh <alex-iskh@users.noreply.github.com>
2017-03-05 16:40:20 +0300

    Change string concatinating to joining

--- rating_export.py
+++ rating_export.py
@@ -11,9 +11,12 @@ from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
 
+def to_string(*args):
+	return ''.join(map(str, args))
+
 def append_to_file(filename, message):
 	with open(filename, 'ab') as file:
-		file.write((message + '\n').encode('utf8'))
+		file.write(to_string(message, '\n').encode('utf8'))
 
 def log(message):
 	print(message)
@@ -32,13 +35,14 @@ def init_browser_with_profile(path_to_profile):
 	return browser
 
 def main():
-	log('Export started: ' + str(datetime.now()))
+	log(to_string('{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{\n',
+		'Export started: ', datetime.now()))
 
 	try:
 		films_table = get_table_from_export_file(sys.argv[1])
 		browser = init_browser_with_profile(sys.argv[2])
 	except FileNotFoundError as ex:
-		log('Error on initialization: ' + ex.filename + ' - ' + ex.strerror)
+		log(to_string('Error on initialization: ', ex.filename, ' - ', ex.strerror))
 		sys.exit()
 
 	imdb = Imdb()
@@ -56,7 +60,7 @@ def main():
 		
 		if rating == '-':
 			log('Film has no rating, skipped')
-			append_to_file(skipped_films_file, title + '|' + title_rus + '|' + year + '|' + rating + '\n')
+			append_to_file(skipped_films_file, to_string(title, '|', title_rus, '|', year, '|', rating, '\n'))
 			continue
 		
 		if not title:
@@ -64,18 +68,18 @@ def main():
 		
 		search_res = imdb.search_for_title(title)
 		
-		log('\n==========================================================')
-		log(str(title) + '\n' + str(title_rus) + '\nYear: ' + str(year) + '\nRating: ' + str(rating))
+		log(to_string('\n==========================================================\n',
+			title, '\n', title_rus, '\nYear: ', year, '\nRating: ', rating))
 		
 		for found_film in search_res:
-			log('\nFound: ' + str(found_film['title']) + ' (' + str(found_film['year']) + ')')
+			log(to_string('\nFound: ', found_film['title'], ' (', found_film['year'], ')'))
 			year_diff = abs(int(year) - int(found_film['year']))
 			if year_diff <= 1:
 				if title != found_film['title'] or year_diff != 0:
 					log('Title is different or year doesn\'t exactly match')
-					append_to_file(mismatches_file, title + '|' + title_rus + '|' + year + '|' + rating + '\n'
-						+ found_film['title'] + ' (' + str(found_film['year']) + ') : '
-						+ 'http://www.imdb.com/title/' + found_film['imdb_id'] + '\n\n')
+					append_to_file(mismatches_file, to_string(title, '|', title_rus, '|', year, '|', rating, '\n',
+						found_film['title'], ' (', str(found_film['year']), ') : ',
+						'http://www.imdb.com/title/', found_film['imdb_id'], '\n\n'))
 				
 				try:
 					browser.get('http://www.imdb.com/title/' + found_film['imdb_id'])
@@ -84,17 +88,18 @@ def main():
 					pass
 				
 				browser.find_element_by_xpath('//*[@data-reactid=".2.0"]').click()
-				browser.find_element_by_xpath('//*[@data-reactid=".2.1.0.1.$' + rating + '"]').click()
+				browser.find_element_by_xpath(to_string('//*[@data-reactid=".2.1.0.1.$', rating, '"]')).click()
 				log('Rated')
 				break
 			else:
 				log('Year does not match at all')
 		else:
 			log('\nFilm not found in the search results, skipped')
-			append_to_file(skipped_films_file, title + '|' + title_rus + '|' + year + '|' + rating + '\n')
+			append_to_file(skipped_films_file, to_string(title, '|', title_rus, '|', year, '|', rating, '\n'))
 
 	browser.quit()
-	log('Export finished: ' + str(datetime.now()))
+	log(to_string('Export finished: ', datetime.now(),
+		'\n}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}'))
 
 if __name__ == "__main__":
 	main()
